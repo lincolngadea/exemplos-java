801993b38b78fe7ce861b26097cbaa134f05895b
Lincoln Gadea <lincoln@glinc.com.br>
2021-01-15 11:50:04 -0300

    chore: finaliza atividade 05

--- Atividades/atividade05/AccountProgram.java
+++ Atividades/atividade05/AccountProgram.java
@@ -1,9 +1,61 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import utils.BankAccount;
 
 public class AccountProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		BankAccount bankAccount;
+		double deposit;
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter account number:");
+		String accountNumber = sc.next();		
+		
+		System.out.print("Enter account Holder:");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		
+		System.out.print("Is there an initial deposit? (y/n)");
+		char firstDeposit = sc.next().charAt(0);
+	
+		if(Character.toUpperCase(firstDeposit) == 'Y') {
+			System.out.print("Enter initial deposit:");
+			deposit = sc.nextDouble();
+			bankAccount = new BankAccount(accountNumber, holder, deposit);
+		}else {
+			bankAccount = new BankAccount(accountNumber, holder);
+		}
+		
+		System.out.println();
+		System.out.print("Account Data:");
+		System.out.print(bankAccount);
+		
+		System.out.println();
+		System.out.print("Enter deposit value:");
+		
+		deposit = sc.nextDouble();
+		bankAccount.deposit(deposit);
+		
+		System.out.println();
+		System.out.print("Update Account Data:");
+		System.out.print(bankAccount);
+		
+		System.out.println();
+		System.out.print("Enter a withdraw value:");
+		
+		double withdraw = sc.nextDouble();
+		bankAccount.withdraw(withdraw);
+		
+		System.out.println();
+		System.out.print("Update Account Data:");
+		System.out.print(bankAccount);
+		
+		sc.close();
 	}
 
 }
--- Atividades/atividade05/utils/BankAccount.java
+++ Atividades/atividade05/utils/BankAccount.java
@@ -3,49 +3,59 @@ package utils;
 public class BankAccount {
 	
 	private String accountNumber;
-	private String cardHolderName;
-	private double deposit;
+	private String holder;
 	private double balance;
-	private double withdraw;
 	
-	private static final double WITHDRAW_TAX = 5.00;
-	
-	public BankAccount(String accountNumber, String cardHolderName, double balance, double withdraw) {
-		
+	public BankAccount(String accountNumber, String holder, double initialDeposit) {
 		this.accountNumber = accountNumber;
-		this.cardHolderName = cardHolderName;
-		this.balance = balance;
-		this.withdraw = withdraw;
+		this.holder = holder;
+		deposit(initialDeposit);
 	}
-	
+
+	public BankAccount(String accountNumber, String holder) {
+		this.accountNumber = accountNumber;
+		this.holder = holder;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
 	public String getAccountNumber() {
 		return accountNumber;
 	}
-	
-	public String getCardHolderName() {
-		return cardHolderName;
-	}
-	public void setCardHolderName(String cardHolderName) {
-		this.cardHolderName = cardHolderName;
-	}
-	public double getDeposit() {
-		return deposit;
-	}
-	public void setDeposit(double deposit) {		
-		this.balance += deposit;		
-		this.deposit = deposit;
-	}
-	public double getWithdraw() {
-		return withdraw;
-	}
-	public void setWithdraw(double withdraw) {		
-		this.withdraw = withdraw;
-		this.balance -= withdraw + WITHDRAW_TAX;
-	}
-	
+
 	public double getBalance() {
 		return balance;
 	}
+	
+	public void deposit(double amount) {		
+		this.balance += amount;		
+	}
+	
+	public void withdraw(double amount) {
+		this.balance -= amount + 5;
+	}
+	
+	public String toString() {
+		return "Account "
+				+ accountNumber
+				+ ", Holder: "
+				+ holder.toUpperCase()
+				+ ", Balance: "
+				+ String.format("%.2f",balance);
+	}
+
+	
+	
+	
+	
+	
+	
 }
 
 
